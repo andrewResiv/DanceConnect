b4fb199391856afd9d91951446abe3b7e9926bd5
Андреев Андрей <a.andreev@npo-pkrv.ru>
2025-02-24 18:49:09 +0300

    Update project and refactoring

--- src/main/java/com/andrew/danceconnect/DanceConnect/config/CorsConfig.java
+++ src/main/java/com/andrew/danceconnect/DanceConnect/config/CorsConfig.java
@@ -1,10 +1,10 @@
 package com.andrew.danceconnect.DanceConnect.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+//Конфиг для работы с frontend
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
     @Override
